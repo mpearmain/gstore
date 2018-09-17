b2e4d0960c4457fd3daea0758cd9cb9afb578038
mpearmain <Casioms90>
2018-09-17 17:20:45 +0800

    more updates

--- src/python/make_trees_dataset.py
+++ src/python/make_trees_dataset.py
@@ -6,30 +6,6 @@ from skopt import forest_minimize, gbrt_minimize, gp_minimize
 from lightgbm import LGBMRegressor
 from ml_metrics import rmse
 
-# Dump cleaned data to parquets for later.
-train_df = pd.read_parquet('input/processed/train_static_features.parquet.gzip')
-test_df = pd.read_parquet('input/processed/test_static_features.parquet.gzip')
-
-# Split for train and validation based on date
-format_str = '%Y%m%d'
-train_df['date'] = train_df['date'].apply(lambda x: datetime.strptime(str(x), format_str))
-split_date ='2017-05-31'
-xtrain = train_df.loc[train_df['date'] <= split_date]
-xvalid = train_df.loc[train_df['date'] > split_date]
-
-# take the log(1+x) of the values for better accuracy.
-xtrain_y = np.log1p(xtrain["totals.transactionRevenue"].values)
-xtrain_id = xtrain["fullVisitorId"].values
-xtrain = xtrain.drop(["totals.transactionRevenue", "fullVisitorId", "date"], axis=1)
-
-xvalid_y = np.log1p(xvalid["totals.transactionRevenue"].values)
-xvalid_id = xvalid["fullVisitorId"].values
-xvalid = xvalid.drop(["totals.transactionRevenue", "fullVisitorId", "date"], axis=1)
-
-y_train = np.log1p(train_df["totals.transactionRevenue"].values)
-train_id = train_df["fullVisitorId"].values
-train_df = train_df.drop(["totals.transactionRevenue", "fullVisitorId", "date"], axis=1)
-
 
 space = space_lightgbm()
 clf = LGBMRegressor(n_jobs=-1, random_state=56, objective='regression', verbose=-1)
